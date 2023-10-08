80407e7beeeae1ae66babf10f45b93bf02e9b87e
uchuvatova <109352814+uchuvatova@users.noreply.github.com>
2023-10-08 18:50:05 +0300

    Добавлен ввод email Убрана проверка ввода даты рождения

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -13,6 +13,10 @@ def test_success_registration_without_email():
     browser.element('#lastName').should(be.blank).type('Uchuvatova')
     # Проверка, что фамилия введена
     browser.element('#lastName').should(have.attribute('value').value_containing('Uchuvatova'))
+    # Ввод email
+    browser.element('#userEmail').should(be.blank).type('example@mail.ru')
+    # Проверка, что фамилия введена
+    browser.element('#userEmail').should(have.attribute('value').value_containing('example@mail.ru'))
     # Скролл до чек-боксов с хобби
     browser.element('label[for=hobbies-checkbox-2]').perform(command.js.scroll_into_view)
     # Выбор пола
@@ -29,8 +33,6 @@ def test_success_registration_without_email():
     browser.element('.react-datepicker__header').should(have.text('April 1986'))
     # Выбор дня
     browser.element('.react-datepicker__day--026').click()
-    # Проверка, что в поле День рождения отображается выбранная дата
-    browser.element('#dateOfBirthInput').should(have.attribute('value').value_containing('26 Apr 1986'))
     # Ввод и выбор Subjects
     browser.element('#subjectsInput').should(be.blank).type('Maths')
     browser.element('#react-select-2-option-0').click()
@@ -59,6 +61,6 @@ def test_success_registration_without_email():
     # Проверка появления модального окна с подтверждением регистрации
     browser.element('.modal-content').element('.modal-header').should(have.text('Thanks for submitting the form'))
     # Проверка соответствия таблицы введенным данным
-    browser.element('.table-responsive').all('td:nth-child(2)').should(have.texts('Ira Uchuvatova', '', 'Female', '1234567890', '26 April,1986', 'Maths', 'Reading', '1.png', 'NN', 'NCR Delhi'))
+    browser.element('.table-responsive').all('td:nth-child(2)').should(have.texts('Ira Uchuvatova', 'example@mail.ru', 'Female', '1234567890', '26 April,1986', 'Maths', 'Reading', '1.png', 'NN', 'NCR Delhi'))
 
 
